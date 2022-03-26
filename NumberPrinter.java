9dd914a1726ae912b65f6d2d6cd33ba042ee4de2
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-03-26 12:02:59 -0400

    Update NumberPrinter.java

--- NumberPrinter.java
+++ NumberPrinter.java
@@ -4,19 +4,17 @@
  */
 public class NumberPrinter 
 {
-
 	public static void main(String[] args) 
 	{
-
 		int i = 0;
 		int MAX = 50;
 		while (i <= MAX) 
 		{
+			/** 
+			 * i%2 == 0 for even number
+			 * i%2 == 1 for odd number
+			 */
 			if (i%2 == 0)	
-				/** 
-				 * i%2 == 0 for even number
-				 * i%2 == 1 for odd number
-				 */
 			{
 				System.out.print(i);
 				
@@ -25,7 +23,8 @@ public class NumberPrinter
 				{
 					System.out.print(",");
 				}				
-			}			
+			}	
+			
 			i++;
 		}
 	}
