0755b71f92dbf6da4e27028ba1ab424ef6c31cb7
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-03-26 11:58:31 -0400

    Add files via upload

---
+++ NumberPrinter.java
@@ -0,0 +1,32 @@
+/**
+ * @author Arash
+ *	This program prints even numbers and stops the comma between them from printing at the end.
+ */
+public class NumberPrinter 
+{
+
+	public static void main(String[] args) 
+	{
+
+		int i = 0;
+		int MAX = 50;
+		while (i <= MAX) 
+		{
+			if (i%2 == 0)	
+				/** 
+				 * i%2 == 0 for even number
+				 * i%2 == 1 for odd number
+				 */
+			{
+				System.out.print(i);
+				
+				int comma = MAX - 1;
+				if(i<comma)
+				{
+					System.out.print(",");
+				}				
+			}			
+			i++;
+		}
+	}
+}
